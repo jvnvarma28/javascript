f455fb66a71cc4e249c5f100859b49a88d04d575
jvnvarma28 <jvnvarma28@gmail.com>
2017-03-02 10:33:54 +0530

    Update addtocart.js

--- addtocart.js
+++ addtocart.js
@@ -1,17 +1,11 @@
-function addtocart()
-{
-   
-   // alert(  $('#mobileshoping_name').attr('ids'))
-    var cart = "";
-    var sideBar = window.localStorage.getItem("cart");
-        if (typeof sideBar !== 'undefined' && sideBar !== null)
-    {
-        //alert(window.localStorage.getItem("cart"))
-        cart = window.localStorage.getItem("cart");
-    }
-        cart = cart + "~" + $('#mobileshoping_name').attr('ids') + "`" + $('#mobileshoping_name').html() + '`' + $('#mobileshoping_price').html() + '`' + $('#mySlidesviewimgmySlides').attr('src');
+<script>
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
 
-    window.localStorage.setItem("cart", cart);
-    checkcheckout();
-    //alert(cart)
-}
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
+</script>
